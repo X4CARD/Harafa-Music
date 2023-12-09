d9811c4f3788bdf03e0a166bf2ea998b6f61898c
X-CARD <x4carding@gmail.com>
2023-12-09 19:41:44 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,68 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "One Love",
+    artist: "Shubh",
+    img: "One Love",
+    src: "music-1"
+  },
+  {
+    name: "Badli Badli Laage",
+    artist: "Sapna",
+    img: "One Love",
+    src: "music-2"
+  },
+  {
+    name: "Chetak",
+    artist: "Sapna",
+    img: "One Love",
+    src: "music-3"
+  },
+  {
+    name: "English Medium",
+    artist: "Sapna",
+    img: "One Love",
+    src: "music-4"
+  },
+  {
+    name: "Teri Lat Lag Jagi",
+    artist: "Sapna",
+    img: "One Love",
+    src: "music-5"
+  },
+  {
+    name: "बन्दूक चलाके",
+    artist: "Sapna",
+    img: "One Love",
+    src: "music-6"
+  },
+  {
+    name: "Teri Aakhya Ka Yo Kajal",
+    artist: "Sapna",
+    img: "One Love",
+    src: "music-7"
+  },
+  {
+    name: "SOLID BODY",
+    artist: "Sapna",
+    img: "One Love",
+    src: "music-8"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
